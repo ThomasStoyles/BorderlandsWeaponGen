5e4700a8b5ddb58071f8edcb60561ff509b42bff
ThomasStoyles <thomasstoyles@gmail.com>
2022-06-29 12:10:00 +0000

    TS: Tests changes for service 2 and 3

--- service1/tests/test_app.py
+++ service1/tests/test_app.py
@@ -19,15 +19,12 @@ class Testindex(TestBase):
 class TestGun1(TestBase):
     def test_gun1(self):
         with requests_mock.Mocker() as m:
-            m.get('http://service2:5000/Manufacturer', text='Jakobs')
-            m.get('http://service3:5000/rarity',text='47')
-            m.post('http://service4:5000/final', json={
-        'Weapon Type': 'Jakobs',
-        'Weapon rarity': 'Rare',
-        'damage type on weapon': 'Ice'
-        })
-
-        response = self.client.get(url_for('index'))
-        self.assert500(response)
-        self.assertIn(b'Weapon Type: Jakobs', response.data.decode)
-
+            m.get('http://service_2:5000/Manufacturer', text='Jakobs')
+            m.get('http://service_3:5000/rarity', text='Uncommon')
+            m.post('http://service_4:5000/final', text='65')
+            
+            response =self.client.get(url_for('index'))
+            self.assert500(response)
+            self.assertIn(b'Jakobs', response.data)
+            self.assertIn(b'Uncommon', response.data)
+            self.assertIn(b'65', response.data)
--- service3/tests/test_app.py
+++ service3/tests/test_app.py
@@ -6,18 +6,11 @@ from unittest.mock import patch
 
 class TestBase(TestCase):
     def create_app(self):
-        return app
+        return app 
 
-
-class TestRarity(TestBase):
-    @patch('application.routes.randint', return_value=123456789)
-    def test_numbs(self, patched):
-        response = self.client.get(url_for('rarity'))
+class Testrarity(TestBase):
+    @patch('application.routes.rd.choice', return_value='Jakobs')
+    def test_mrarity(self, patched):
+        response =self.client.get(url_for('rarity'))
         self.assert200(response)
-        self.assertIn(b'123456789', response.data)
-
-    @patch('application.routes.randint', return_value=987654321)
-    def test_numbsV2(self, patched):
-        response = self.client.get(url_for('rarity'))
-        self.assert200(response)
-        self.assertIn(b'987654321', response.data)
+        self.assertIn(b'Jakobs', response.data)

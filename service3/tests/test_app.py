f737a39a68526947a0b58fadc5ea73e8ebdadb27
ThomasStoyles <thomasstoyles@gmail.com>
2022-07-06 11:45:38 +0000

    TS: Test 3 changes

--- service3/tests/test_app.py
+++ service3/tests/test_app.py
@@ -9,8 +9,8 @@ class TestBase(TestCase):
         return app 
 
 class Testrarity(TestBase):
-    @patch('application.routes.rd.choice', return_value='Jakobs')
+    @patch('application.routes.rd.choice', return_value='Uncommon')
     def test_mrarity(self, patched):
         response =self.client.get(url_for('rarity'))
         self.assert200(response)
-        self.assertIn(b'Jakobs', response.data)
+        self.assertIn(b'Uncommon', response.data)

4ad93934cdbcd7655f340a7ff307f026fed2aae8
ThomasStoyles <thomasstoyles@gmail.com>
2022-07-11 10:06:11 +0000

    TS: Changes

--- service2/application/routes.py
+++ service2/application/routes.py
@@ -1,7 +1,7 @@
 from application import app
 import random as rd
 
-Manufacturers = ['Atlas','Hyperion','Jakobs','Maliwan','Anshin', 'Vladof', 'a', 'b', 'c']
+Manufacturers = ['Atlas','Hyperion','Jakobs','Maliwan','Anshin', 'Vladof']
 
 @app.route('/Manufacturer', methods=['GET'])
 def Manufacturer():
